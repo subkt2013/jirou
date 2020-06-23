9396124559f1041a83651d9125173b1815333ad9
subkt2013@gmail.com <tate@sbicb.co.jp>
2020-06-23 13:33:18 +0900

    DBの直近で使わないものを削除

--- database/migrations/2020_06_17_081252_create_stores_table.php
+++ database/migrations/2020_06_17_081252_create_stores_table.php
@@ -16,10 +16,7 @@ class CreateStoresTable extends Migration
         Schema::create('stores', function (Blueprint $table) {
             $table->bigInteger('id',true)->unsigned();
             $table->string('store_name')->unique();
-            $table->string('image_path')->unique()->comment('[イメージの格納先(src)]');
-            $table->string('alt')->comment('[画像の説明]');
             $table->bigInteger('wait_people')->unsigned();
-            $table->bigInteger('estemated_wait_time')->unsigned();
             $table->text('introduction', 65535)->nullable()->comment('[店舗紹介]');
             $table->timestamp('created_at')->nullable();
         });
